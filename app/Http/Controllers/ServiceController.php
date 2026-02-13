2b09e0e14bb8a0ebe55d0f93ab8e6a3718b926a7
youssef boualy <youssefboua913@gmail.com>
2026-02-13 16:33:03 +0100

    add crud of service

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ServiceRequest;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $services = Service::all();
+        // return view('service.index',compact('services'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // return view('service.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ServiceRequest $request)
+    {
+        $data = $request->validated();
+        Service::create($data);
+    // redirect()->route('service.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Service $service)
+    {
+        //return view('service.show',compacte('servise'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Service $service)
+    {
+        //return view('service.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ServiceRequest $request, Service $service)
+    {
+         $data = $request->validated();
+        $service->update($data);
+    // redirect()->route('service.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Service $service)
+    {
+        $service->delete();
+        // redirect()->route('service.index');
+    }
+}
